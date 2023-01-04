3685ace8ebbd056f472a96a1ef8f0dcde4eb3672
Kelly Freitas <kellyvfreitas@live.com>
2023-01-04 17:35:11 -0300

    Inicialização do Projeto Sistema de Finanças Pessoais.

---
+++ src/data/items.ts
@@ -0,0 +1,38 @@
+export const items = [
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "Faculdade",
+    value: 300.85,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "BurguerYou",
+    value: 46.18,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "Cartão",
+    value: 3600.8,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "Mercantil",
+    value: 700.8,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "SalarioCFY",
+    value: 1400.0,
+  },
+  {
+    date: new Date(2021, 9, 15),
+    category: "food",
+    title: "SalarioTrixx",
+    value: 1000.0,
+  },
+];
